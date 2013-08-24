e3e15e1ff68171702e5d601804c3f9ddfe2adf84
Akira Ouchi <akkiesoft@marokun.net>
2013-08-25 02:30:14 +0900

    設定を追加
    ・ソースを書き換えなくても嫁画像が編集可能
    ・妹・妻などの選択が可能になった
    いずれも要mikutter再起動

--- mikutter_yometab.rb
+++ mikutter_yometab.rb
@@ -5,12 +5,38 @@
 ##
 
 Plugin.create :mikuttre_yometab do
-  tab(:mikutter_yometab, '嫁タブ') do
-    icon = File.expand_path(File.join(File.dirname(__FILE__), 'yome.png'))
-    set_icon icon
 
+  yometab_table = {
+    	'yome'		=> '嫁',
+	   	'tsuma'		=> '妻',
+    	'musume'	=> '娘',
+    	'imouto'	=> '妹',
+    	'kanojo'	=> '彼女',
+    	'jis'		=> '〄'
+  }
+
+  yometab_name = UserConfig[:yometab_name]
+  if yometab_name == '' then yometab_name = "yome" end
+  yome_url = UserConfig[:yometab_url]
+  yome_x = UserConfig[:yometab_x]
+  if yome_x == '' then yome_x = "100" end
+  yome_y = UserConfig[:yometab_y]
+  if yome_y == '' then yome_y = "100" end
+  
+  tab(:mikutter_yometab, "#{yometab_table[yometab_name]}タブ") do
+    icon = File.expand_path(File.join(File.dirname(__FILE__), "#{yometab_name}.png"))
+    set_icon icon
     expand
-    picture = Gtk::WebIcon.new("http://cdn-ak.f.st-hatena.com/images/fotolife/A/Akkiesoft/20120124/20120124015632.png",400, 654)
-    nativewidget Gtk::HBox.new(false, 0).closeup(picture)
+    if yome_url != "" then
+      picture = Gtk::WebIcon.new(yome_url, yome_x.to_i, yome_y.to_i)
+      nativewidget Gtk::HBox.new(false, 0).closeup(picture)
+    end
+  end
+
+  settings "嫁タブ" do
+    input '画像URL', :yometab_url
+    input '画像Xサイズ', :yometab_x
+    input '画像Yサイズ', :yometab_y
+    select 'タブ名', :yometab_name, yometab_table
   end
 end
